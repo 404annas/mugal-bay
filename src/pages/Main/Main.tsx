87f1827d864710bf9e9766ca0d1343f1326d859d
Muhammad Annas Khan <annasking601@gmail.com>
2026-06-03 13:39:32 +0500

    dog section done

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,17 +1,16 @@
-import Cards from '@/components/Home/Cards'
-import FocusSlider from '@/components/Home/FocusSlider'
-import Hero from '@/components/Home/Hero'
-import Hero2 from '@/components/Home/Hero2'
+import DogSection from "@/components/Home/DogSection";
+import Hero2 from "@/components/Home/Hero2";
 
 const Main = () => {
   return (
     <div>
       {/* <FocusSlider /> */}
-        
-        <Hero2 />
-        {/* <Cards /> */}
-    </div>
-  )
-}
 
-export default Main
+      <Hero2 />
+      <DogSection />
+      {/* <Cards /> */}
+    </div>
+  );
+};
+
+export default Main;
